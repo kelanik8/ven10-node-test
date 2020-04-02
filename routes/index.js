b55a76bfbb77f445ca17c8818c1bcb1f00bb4a98
Kelani Tolulope <kelanik8@gmail.com>
2020-04-02 20:00:27 +0100

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,42 @@
 const { Router } = require("express");
+const fetch = require("node-fetch");
 const filtersData = require("../API/filters.json");
 const carOwnersData = require("../API/car_owners_data.json");
 const router = Router();
 
-router.get("/filter/:id", async (req, res) => {
-  let filteredData = filtersData.find(filter => filter.id == req.params.id);
+router.get("/filter/:id", (req, res) => {
+  fetch("https://ven10.co/assessment/filter.json", { method: "Get" })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let filteredData = data.find(filter => filter.id == req.params.id);
 
-  if (!filteredData) {
-    return res.status(500).json({ message: "Id is invalid", data: null });
-  }
+      if (!filteredData) {
+        return res.status(500).json({ message: "Id is invalid", data: null });
+      }
 
-  let carOwners = await carOwnersData.filter(
-    data =>
-      +data.car_model_year >= +filteredData.start_year &&
-      +data.car_model_year <= +filteredData.end_year
-  );
+      let carOwners = carOwnersData.filter(
+        data =>
+          +data.car_model_year == +filteredData.start_year ||
+          +data.car_model_year == +filteredData.end_year
+      );
 
-  res.status(200).json({
-    message: "Car owners data retrieved successfully",
-    data: carOwners.slice(0, 30)
-  });
+      res.status(200).json({
+        message: "Car owners data retrieved successfully",
+        data: carOwners.slice(0, 30)
+      });
+    });
 });
 
 router.get("/filters", async (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Filters retrieved successfully", data: filtersData });
+  fetch("https://ven10.co/assessment/filter.json", { method: "Get" })
+    .then(res => res.json())
+    .then(json => {
+      let filtersData = json;
+      return res
+        .status(200)
+        .json({ message: "Filters retrieved successfully", data: filtersData });
+    });
 });
 
 module.exports = router;
